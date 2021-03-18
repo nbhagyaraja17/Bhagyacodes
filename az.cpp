db872023781126d0b5fe4eab070e6ec16965b5bc
Bhagya raja <nbhagyaraja17@gmail.com>
2021-03-18 14:58:04 +0530

    Square fence my approach

--- az.cpp
+++ az.cpp
@@ -1,59 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+#define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using ll = long long;
 
-vector<ll> edges(4, 0);
-ll n, sum;
+const int mod = 1e9 + 7;
 
-// chinni said this is a brute force templete
-// he took 4 edges and keep on add side to edge , every edge must add to any of the edge. If any side is
-// not added to any edge that means NO  .
-bool solve(vector<ll> &a, ll start)
+int n;
+ll sum, ans;
+
+// my approach generate possible subsets and if s == sum/4, ans++, then prune it off and then do it 
+// 4 for 4 square sides if my ans is not 4 then NO.
+void brute(vector<ll> a)
 {
-    if (start == n)
+    ll s;
+    for (int k = 0; k < 4; k++) //3*N*2^N + N ==> 1.5*1e6 approx.
     {
-        return true;
-    }
-
-    for (int i = 0; i < 4; i++)
-    {
-        if (edges[i] + a[start] <= sum / 4)
+        if (k < 3)
+            for (ll i = 1; i < (1LL << (ll)a.size()); i++)
+            {
+                s = 0;
+                vector<int> del;
+                for (ll j = 0; j < (ll)a.size(); j++)
+                {
+                    if ((i & (1LL << j)) && a[j])
+                    {
+                        s += a[j];
+                        del.push_back(j);
+                    }
+                }
+                if (s == sum / 4)
+                {
+                    ans++;
+                    for (auto x : del)
+                    {
+                        a[x] = 0;
+                    }
+                }
+            }
+        else
         {
-            edges[i] += a[start];
-            if (solve(a, start + 1))
-                return true;
-            else
-                edges[i] -= a[start];
+            s = 0;
+            for (int j = 0; j < (int)a.size(); j++)
+            {
+                s += a[j];
+            }
+            if (s == sum / 4)
+                ans++;
         }
     }
-    return false;
+}
+void solve()
+{
+    cin >> n;
+    vector<ll> a(n);
+    sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        sum += a[i];
+    }
+    if (sum % 4 != 0)
+    {
+        cout << "NO\n";
+        return;
+    }
+    brute(a);
+    cout << (ans == 4 ? "YES\n" : "NO\n");
 }
 
 int main()
 {
-    ll t;
+    fast;
+    int t = 1;
     cin >> t;
     while (t--)
     {
-        cin >> n;
-        sum = 0;
-        vector<ll> a(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-            sum += a[i];
-        }
-        if (sum % 4)
-            cout << "NO" << endl;
-        else
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                edges[i] = 0;
-            }
-            cout << (solve(a, 0) ? "YES" : "NO") << endl;
-        }
+        solve();
     }
-
     return 0;
 }
