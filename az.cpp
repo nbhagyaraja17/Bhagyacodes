463299b1c92a9b81614dbf8d177b5737c2f47c23
Bhagya raja <nbhagyaraja17@gmail.com>
2021-05-01 17:20:37 +0530

    Prime palindrome pbm

--- az.cpp
+++ az.cpp
@@ -7,9 +7,6 @@ using ll = long long;
 
 const int mod = 1e9 + 7;
 
-// segment tree structure
-// T.C for updating logn , for sum logn so.. overall O(QlogN)
-
 template <class T>
 void print_data(T &a)
 {
@@ -35,71 +32,93 @@ ll power(ll x, ll y)
     return ans;
 }
 
-vector<int> arr(100100);
-vector<int> tree(400400);
-int n, q;
+string s;
+ll cnt, a, b, num, len;
 
-void build(int index, int l, int r)
+// 1 2  3   4
+// 1 1  2   2
+// 1 11 111 1111
+bool check_prime() // O(sqrt(n))
 {
-    if (l == r)
+    string temp = s;
+    string dup;
+    if (s.length() >= 1)
     {
-        tree[index] = arr[l];
-        return;
-    }
-    ll mid = (l + r) / 2;
-    build(2 * index, l, mid);
-    build(2 * index + 1, mid + 1, r);
-    tree[index] = tree[2 * index] + tree[2 * index + 1];
-}
-
-void update(int index, int l, int r, int x, int v)
-{
-    if (x < l || r < x)
-        return;
-    if (l == r)
-    {
-        tree[index] = v;
-        return;
-    }
-    ll mid = (l + r) / 2;
-    update(2 * index, l, mid, x, v);
-    update(2 * index + 1, mid + 1, r, x, v);
-    tree[index] = tree[2 * index] + tree[2 * index + 1];
-}
-
-ll sum_query(int index, int l, int r, int pleft, int pright)
-{
-    if (r < pleft || pright < l)
-        return 0;
-    if (pleft <= l && pright >= r)
-        return tree[index];
-    int mid = (l + r) / 2;
-    return (sum_query(2 * index, l, mid, pleft, pright) + sum_query(2 * index + 1, mid + 1, r, pleft, pright));
-}
-
-void solve()
-{
-    cin >> n >> q;
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    build(1, 0, n - 1);
-    while (q--)
-    {
-        int ch;
-        cin >> ch;
-        if (ch == 1)
+        if (len % 2)
         {
-            int x, v;
-            cin >> x >> v;
-            update(1, 0, n - 1, x, v);
+            dup = s.substr(0, s.length() - 1);
+            reverse(dup.begin(), dup.end());
         }
         else
         {
-            int l, r;
-            cin >> l >> r;
-            cout << sum_query(1, 0, n - 1, l, r) << "\n";
+            dup = s;
+            reverse(dup.begin(), dup.end());
         }
+        temp += dup;
+        num = stoll(temp);
+        //cout << "debg : " << num << "\n";
+        if (num < 2)
+            return false;
+        if (num == 2 && (a <= num) && (num <= b))
+            return true;
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0)
+                return false;
+        }
+        return (true && (a <= num) && (num <= b));
     }
+}
+
+// 1 0 0 1 1 0 0 1
+//
+void brute(int level, int n)
+{
+    if (num > b)
+        return;
+    if (level == n)
+    {
+        if (check_prime())
+        {
+            cnt++;
+        }
+        return;
+    }
+    for (int choice = 0; choice <= 9; choice++)
+    {
+        if (s.empty() && choice == 0)
+            continue;
+        s.push_back(choice + '0');
+        brute(level + 1, n);
+        s.pop_back();
+    }
+}
+
+void solve() // O(5*5!*sqrt(n)*c)
+{
+    cin >> a >> b;
+    ll temp1 = a;
+    ll temp2 = b;
+    int len1(0), len2(0);
+    while (temp1)
+    {
+        len1++;
+        temp1 /= 10;
+    }
+    while (temp2)
+    {
+        len2++;
+        temp2 /= 10;
+    }
+    //
+    for (int i = len1; i <= len2; i++)
+    {
+        len = i;
+        ll hlen = (i + 1) / 2;
+        brute(0, hlen);
+    }
+    //cout << "ans is: ";
+    cout << cnt << "\n";
 }
 
 int main()
