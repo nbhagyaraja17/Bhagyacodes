826c724d8b9915578d13d7ae4c85760deeb44460
Bhagya raja <nbhagyaraja17@gmail.com>
2021-05-04 20:27:53 +0530

    Minimum operation conversion -greedy pbm

--- az.cpp
+++ az.cpp
@@ -7,125 +7,34 @@ using ll = long long;
 
 const int mod = 1e9 + 7;
 
-template <class T>
-void print_data(T &a)
+void solve()
 {
-    for (auto it = a.begin(); it != a.end(); it++)
+    ll x, y;
+    cin >> x >> y;
+    ll op = 0;
+    if (x >= y)
     {
-        cout << *it << " ";
+        cout << (x - y) << "\n";
     }
-    cout << "\n";
-}
-ll power(ll x, ll y)
-{
-    ll temp;
-    ll ans = 1;
-    while (y)
+    else
     {
-        if (y & 1)
+        while (y > x)
         {
-            ans = (1LL * ans * x) % mod;
+            if (y & 1)
+                y++;
+            else
+                y /= 2;
+            op++;
         }
-        x = (1LL * x * x) % mod;
-        y /= 2;
+        cout << op + (x - y) << "\n";
     }
-    return ans;
-}
-
-string s;
-ll cnt, a, b, num, len;
-
-// 1 2  3   4
-// 1 1  2   2
-// 1 11 111 1111
-bool check_prime() // O(sqrt(n))
-{
-    string temp = s;
-    string dup;
-    if (s.length() >= 1)
-    {
-        if (len % 2)
-        {
-            dup = s.substr(0, s.length() - 1);
-            reverse(dup.begin(), dup.end());
-        }
-        else
-        {
-            dup = s;
-            reverse(dup.begin(), dup.end());
-        }
-        temp += dup;
-        num = stoll(temp);
-        //cout << "debg : " << num << "\n";
-        if (num < 2)
-            return false;
-        if (num == 2 && (a <= num) && (num <= b))
-            return true;
-        for (int i = 2; i * i <= num; i++)
-        {
-            if (num % i == 0)
-                return false;
-        }
-        return (true && (a <= num) && (num <= b));
-    }
-}
-
-// 1 0 0 1 1 0 0 1
-//
-void brute(int level, int n)
-{
-    if (num > b)
-        return;
-    if (level == n)
-    {
-        if (check_prime())
-        {
-            cnt++;
-        }
-        return;
-    }
-    for (int choice = 0; choice <= 9; choice++)
-    {
-        if (s.empty() && choice == 0)
-            continue;
-        s.push_back(choice + '0');
-        brute(level + 1, n);
-        s.pop_back();
-    }
-}
-
-void solve() // O(5*5!*sqrt(n)*c)
-{
-    cin >> a >> b;
-    ll temp1 = a;
-    ll temp2 = b;
-    int len1(0), len2(0);
-    while (temp1)
-    {
-        len1++;
-        temp1 /= 10;
-    }
-    while (temp2)
-    {
-        len2++;
-        temp2 /= 10;
-    }
-    //
-    for (int i = len1; i <= len2; i++)
-    {
-        len = i;
-        ll hlen = (i + 1) / 2;
-        brute(0, hlen);
-    }
-    //cout << "ans is: ";
-    cout << cnt << "\n";
 }
 
 int main()
 {
     fast;
     int t = 1;
-    //cin >> t;
+    cin >> t;
     while (t--)
     {
         solve();
