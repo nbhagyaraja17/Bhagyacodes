635c091da761fedcd0bfc9533bc576f11ac1669e
Bhagya raja <nbhagyaraja17@gmail.com>
2021-05-13 15:09:35 +0530

    atcoder vacation pbm

--- az.cpp
+++ az.cpp
@@ -7,146 +7,54 @@ using ll = long long;
 
 const int mod = 1e9 + 7;
 
-// form 0-1
-
-/*int n, b, k; 
-vector<int> p;
-vector<int> h;
-int dp[1010][1010];
-int back[1010][1010];
-
-int rec(int level, int wleft)
+int n;
+struct que
 {
-    if (wleft < 0)
-        return INT_MIN;
-    if (level == n)
-    {
+    int a, b, c;
+};
+
+vector<que> arr;
+int dp[100100][4];
+
+int rec(int level, int choice)
+{
+    if (level >= n)
         return 0;
-    }
-    if (dp[level][wleft] != -1)
-        return dp[level][wleft];
-    int ans = rec(level + 1, wleft);
-    int ans1 = h[level] + rec(level + 1, wleft - p[level]);
-    if (ans > ans1)
+    if (dp[level][choice] != -1)
+        return dp[level][choice];
+    int ans = 0;
+    int temp = choice;
+    if (choice != 1)
     {
-        back[level][wleft] = 0;
+        choice = 1;
+        ans = max(ans, arr[level].a + rec(level + 1, choice));
+        choice = temp;
     }
-    else if (ans1 > ans)
+    if (choice != 2)
     {
-        ans = ans1;
-        back[level][wleft] = 1;
+        choice = 2;
+        ans = max(ans, arr[level].b + rec(level + 1, choice));
+        choice = temp;
     }
-    else
+    if (choice != 3)
     {
-        back[level][wleft] = 2;
+        choice = 3;
+        ans = max(ans, arr[level].c + rec(level + 1, choice));
+        choice = temp;
     }
-    return dp[level][wleft] = ans;
+    return dp[level][choice] = ans;
 }
 
-vector<vector<int>> allsol;
-vector<int> temp;
-
-void getsol(int level, int pricepaid)
-{
-    if (back[level][pricepaid] == -1)
-    {
-        allsol.push_back(temp);
-        return;
-    }
-    else if (back[level][pricepaid] == 0)
-    {
-        getsol(level + 1, pricepaid);
-    }
-    else if (back[level][pricepaid] == 1)
-    {
-        temp.push_back(level);
-        getsol(level + 1, pricepaid - p[level]);
-        temp.pop_back();
-    }
-    else
-    {
-        getsol(level + 1, pricepaid);
-        temp.push_back(level);
-        getsol(level + 1, pricepaid - p[level]);
-        temp.pop_back();
-    }
-}*/
-
-// form 2 ending level max sum in subarray.
-
-/*int n;
-vector<int> arr;
-int dp[100100];
-int done[100100];
-
-int rec(int level)
-{
-    if (level < 0)
-        return 0;
-    if (done[level] != -1)
-        return dp[level];
-    int ans = max(arr[level], arr[level] + rec(level - 1));
-    done[level] = 1;
-    return dp[level] = ans;
-}
-/*vector<vector<int>> allsol;
-vector<int> temp;
-void getsol(int level)
-{
-    if (back[level] == -1)
-    {
-        allsol.push_back(temp);
-        return;
-    }
-    else if (back[level] == 0)
-    {
-        getsol(level + 1);
-    }
-    else if (back[level] == 1)
-    {
-        temp.push_back(level);
-        getsol(level + 1);
-        temp.pop_back();
-    }
-    else
-    {
-        getsol(level - 1);
-        temp.push_back(level);
-        getsol(level + 1);
-        temp.pop_back();
-    }
-}*/
-
-// form 3 LCS longest common subsequence
-int n1, n2;
-string s1, s2;
-int dp[1010][1010];
-
-int rec(int level1, int level2)
-{
-    if (level1 >= n1 || level2 >= n2)
-        return 0;
-    if (dp[level1][level2] != -1)
-        return dp[level1][level2];
-    int ans = max(rec(level1 + 1, level2), rec(level1, level2 + 1));
-    if (s1[level1] == s2[level2])
-    {
-        ans = max(ans, rec(level1 + 1, level2 + 1) + 1);
-    }
-    return dp[level1][level2] = ans;
-}
 void solve()
 {
-    cin >> n1 >> n2;
-    cin >> s1 >> s2;
+    cin >> n;
+    arr.resize(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i].a >> arr[i].b >> arr[i].c;
+    }
     memset(dp, -1, sizeof(dp));
-    cout << rec(0, 0);
-    // for (auto v : allsol)
-    // {
-    //     for (auto x : v)
-    //         cout << x << " ";
-    //     cout << "\n";
-    // }
+    cout << rec(0, 0) << "\n";
 }
 
 int main()
