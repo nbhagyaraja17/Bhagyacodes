1997218969771b76d5e8a9432a614a160146da4b
Bhagya raja <nbhagyaraja17@gmail.com>
2021-07-07 17:05:47 +0530

    Painter partition pbm

--- practice.cpp
+++ practice.cpp
@@ -1,30 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using ll = long long;
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
 
-int SuperFrequentElement(vector<int> &v)
+const int mod = 1e9 + 7;
+
+vector<int>arr;
+int n, k;
+int F(int t)
 {
-    // Complete the function
-    // boyer moores voting algorithm
-    int count = 0;
-    int ans = v[0];
-    for(int i = 0 ; i<v.size(); i++){
-        if(count == 0)ans = v[i];
-        if(ans == v[i])count++;
-        else count--;
+    long long sumtemp = 0;
+    long long cnt = 0;
+    for(int i = 0; i<n; i++){
+        sumtemp += arr[i];
+        if(sumtemp > t){
+            sumtemp = 0;
+            i--;
+            cnt++;
+        }
     }
-    return ans;
+    return cnt<=k;
+}
+
+void solve()
+{
+    cin >> n >> k;
+    arr.resize(n);
+    ll sum = 0;
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+        sum+=arr[i];
+    }
+    int lo =  *max_element(arr.begin(),arr.end());
+    int hi = sum;
+    long long ans = 0;
+    while(lo < hi){
+        int mid = lo + (hi - lo)/2;
+        if(F(mid)){
+            ans = mid;
+            hi = mid-1;
+        }
+        else{
+            lo = mid + 1;
+        }
+    }
+    while()
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(NULL);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (auto &i : v)
-        cin >> i;
-
-    cout << SuperFrequentElement(v) << "\n";
+    fast;
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
