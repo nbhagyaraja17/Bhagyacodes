047455b4665fff3c81fdce88cf60034d13d9297e
Bhagya raja <nbhagyaraja17@gmail.com>
2021-12-31 12:34:14 +0530

    meet in the middle example0

--- az.cpp
+++ az.cpp
@@ -8,44 +8,41 @@ using ll = long long;
 #define deb(x) cout << #x << "=" << x << endl
 #define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
 
+// ques: given an array. Find no. of subsets whose sum is <= x (including empty subset)
 const int mod = 1e9 + 7;
-
-// abcd
-// a + rec(bcd)--> b + rec(cd)--> c + rec(d) cd dc 
-//                 c + rec(bd)--> b + rec(d) bd db
-//                 d + rec(bc)--> b + rec(c) bc cb
-// b + rec(acd)
-// 
-// 
-// c + rec(bad)
-// 
-// 
-// d + rec(bca)
-// 
-// 
-int n;
-int c;
-set<string>st;
-void rec(int level, string s)
-{
-    if(level == s.size())
-    {
-        cout << s << "\n";
-        return;
+int n, x;
+vector<int>arr;
+vector<int>gen(vector<int> a){
+    vector<int>b;
+    for(int i = 0; i < (1 << a.size()); i++){
+        int sum = 0;
+        for(int j = 0; j<a.size(); j++){
+            if((1 << j) & i)sum += a[j];
+        }
+        b.push_back(sum);
     }
-    for(int i = level; i<s.size(); i++)
-    {
-        swap(s[level], s[i]);
-        rec(level+1, s);
-        swap(s[level], s[i]);
+    return b;
+}
+int answer(){
+    vector<int>newarr[2];
+    for(int i = 0; i<n; i++){
+        newarr[i&1].push_back(arr[i]);
     }
+    vector<int>sub0, sub1;
+    sub0 = gen(newarr[0]);
+    sub1 = gen(newarr[1]);
+    int cnt = 0;
+    for(auto v : sub0){
+        cnt += upper_bound(sub1.begin(), sub1.end(), x-v) - sub1.begin();
+    }
+    return cnt;
 }
 void solve()
 {
-    string s; cin >> s;
-    n = s.length();
-    rec(0, s);
-    // cout << c << "\n";
+    cin >> n >> x;
+    arr.resize(n);
+    for(auto &i: arr)cin >> i;
+    cout << answer() << "\n";
 }
 
 signed main()
