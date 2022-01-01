1acf9f9ecaa139d67be9696fb0c45d014e80f73f
Bhagya raja <nbhagyaraja17@gmail.com>
2022-01-01 16:45:51 +0530

    meet in the middle ex1 (foursome)

--- practice.cpp
+++ practice.cpp
@@ -8,56 +8,41 @@ using ll = long long;
 #define deb(x) cout << #x << "=" << x << endl
 #define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
 
-// ques: given an array. Find no. of subsets whose sum is <= x (including empty subset)
 const int mod = 1e9 + 7;
-int n, x;
-vector<int> arr;
-vector<int> gen(vector<int> a)
-{
-    vector<int> b;
-    for (int i = 0; i < (1 << a.size()); i++)
-    {
-        int sum = 0;
-        for (int j = 0; j < a.size(); j++)
-        {
-            if ((1 << j) & i)
-                sum += a[j];
-        }
-        b.push_back(sum);
-    }
-    return b;
-}
-int answer()
-{
-    vector<int> newarr[2];
-    for (int i = 0; i < n; i++)
-    {
-        newarr[i & 1].push_back(arr[i]);
-    }
-    vector<int> sub0, sub1;
-    sub0 = gen(newarr[0]);
-    sub1 = gen(newarr[1]);
-    int cnt = 0;
-    for (auto v : sub0)
-    {
-        cnt += upper_bound(sub1.begin(), sub1.end(), x - v) - sub1.begin();
-    }
-    return cnt;
-}
+
+// find is it possible to have four indices a, b, c, d (repetition allowed) 
+// such that arr[a] + arr[b] + arr[c] + arr[d] == target
+
+// idea : A + B + C + D == target.. splitting A + B to X and C + D to Y
+// getting all the possible A + B i.e, X and mapping them and then checking if any tar - Y presents in our map
+// if presents then that will be our X 
 void solve()
 {
-    cin >> n >> x;
-    arr.resize(n);
-    for (auto &i : arr)
-        cin >> i;
-    cout << answer() << "\n";
+    int n, tar; cin >> n >> tar;
+    vector<int>arr(n);
+    for(auto &i: arr)cin >> i;
+    map<int,int>mp;
+    for(int i = 0; i<n; i++){
+        for(int j = i; j<n; j++){
+            mp[arr[i] + arr[j]] = 1;
+        }
+    }
+    for(int i = 0; i<n; i++){
+        for(int j = i; j<n; j++){
+            if(mp[tar - (arr[i] + arr[j])] == 1){
+                cout << "YES\n";
+                return;
+            }
+        }
+    }
+    cout << "NO\n";
 }
 
 signed main()
 {
     fast;
     int t = 1;
-    // cin >> t;
+    //cin >> t;
     while (t--)
     {
         solve();
