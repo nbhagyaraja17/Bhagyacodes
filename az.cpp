fd517e1daa0f0ac115cf30d16aef45f91e2a1a69
Bhagya raja <nbhagyaraja17@gmail.com>
2022-04-06 01:16:53 +0530

    max area of histogram in binary matix

--- az.cpp
+++ az.cpp
@@ -10,34 +10,68 @@ using ll = long long;
 
 const int mod = 1e9 + 7;
 
-// max area(rect) histogram problem
+// given : binary matrix
+// Cal : max area of histogram containing 1s.
+vector<int>left_smaller_index(vector<int>arr){
+    int n = (int)arr.size();
+    vector<int>ans(n);
+    stack<int>temp;
+    for(int i = 0; i<n; i++){
+        while(!temp.empty() && arr[temp.top()] >= arr[i])temp.pop();
+        if(temp.empty())ans[i] = -1;
+        else ans[i] = temp.top();
+        temp.push(i);
+    }
+    return ans;
+}
+vector<int>right_smaller_index(vector<int>arr){
+    int n = (int)arr.size();
+    vector<int>ans(n);
+    stack<int>temp;
+    for(int i = n-1; i>= 0; i--){
+        while(!temp.empty() && arr[temp.top()] >= arr[i])temp.pop();
+        if(temp.empty())ans[i] = n;
+        else ans[i] = temp.top();
+        temp.push(i);
+    }
+    return ans;
+}
 void solve()
 {
-    int n; cin >> n;
-    vector<int>arr(n);
-    for(auto &i : arr)cin >> i;
-    stack<int>temp;
-    vector<int>left(n),right(n);
-    for(int i  = 0; i<n; i++){
-        while(!temp.empty() && arr[temp.top()] >= arr[i])temp.pop();
-        if(temp.empty())left[i] = i+1;
-        else left[i] = i - temp.top();
-        temp.push(i);
+    int m, n; cin >> m >> n;
+    vector<vector<int>>mat(m, vector<int>(n)), cal(m, vector<int>(n, 0));
+    for(auto &v : mat){
+        for(auto &i : v)cin >> i;
     }
-    while(!temp.empty())temp.pop();
-    for(int i  = n-1; i>=0; i--){
-        while(!temp.empty() && arr[temp.top()] >= arr[i])temp.pop();
-        if(temp.empty())right[i] = n-i;
-        else right[i] = temp.top() - i;
-        temp.push(i);
+    for(int j = 0; j<n; j++){
+        for(int i = 0 ;i<m; i++)
+        {
+            if(mat[i][j] == 0)continue;
+            if(i == 0)
+            {
+                cal[i][j] = max(cal[i][j], mat[i][j]);
+                continue;
+            }
+            cal[i][j] = max(cal[i-1][j], mat[i-1][j]) + 1;
+        }
     }
-    // for(auto i : left)cout << i << " ";
-    // cout << "\n";
-    // for(auto i : right)cout << i << " ";
-    // cout << "\n";
+    // for(auto v : cal){
+    //     for(auto i : v)cout << i << " ";
+    //     cout << "\n";
+    // }
     int ans = 0;
-    for(int i = 0; i<n; i++){
-        ans = max(ans, (left[i]+right[i]-1)*arr[i]);
+    for(int i = 0; i<m; i++){
+        vector<int>temp;
+        for(int j = 0; j<n; j++)
+        {
+            temp.push_back(cal[i][j]);
+        }
+        vector<int>lns = left_smaller_index(temp);
+        vector<int>rns = right_smaller_index(temp);
+        for(int i = 0 ;i<n; i++){
+            // deb2(rns[i], lns[i]);
+            ans = max(ans, (rns[i]-lns[i]-1)*temp[i]);
+        }
     }
     cout << ans << "\n";
 }
