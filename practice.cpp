e4e70479d1b93bbadd7115d21bdc686122dfd370
Bhagya <nbhagyaraja17@gmail.com>
2022-06-29 23:30:22 +0530

    2 pointer template

--- practice.cpp
+++ practice.cpp
@@ -11,25 +11,35 @@ using ll = long long;
 
 const int mod = 1e9 + 7;
 
-bool nextPermutation(string &nums) 
-{
-    if(nums.empty()) return false;
-    int i=nums.size()-1;
-    while(i>=1 && nums[i]<=nums[i-1]) i--;
-    if(i==0) return false; // no next permutation, i.e. already largest
-    
-    int j=nums.size()-1;
-    while(nums[j]<=nums[i-1]) j--;
-    swap(nums[i-1],nums[j]);
-    
-    reverse(nums.begin()+i,nums.end());
-    return true;
-}
 void solve()
 {
-    string s; cin >> s;
-    if(nextPermutation(s))cout << s << "\n";
-    else cout << "-1\n";
+    int n, k; cin >> n >> k;
+    vi arr(n);
+    for(auto &i : arr)cin >> i;
+    int tail = 0;
+    int head = -1; // imp for empty subarray
+    int ans = 0;
+    int cntzero = 0;
+    while(tail < n){
+        // push head as far as possible
+        while(head + 1 < n && (arr[head + 1] == 1 or (arr[head + 1] == 0 and cntzero < k)))
+        {
+            head++;
+            if(arr[head] == 0)cntzero++;
+        }
+        // updating ans
+        ans = max(ans, (head - tail + 1));
+        // move tail one step
+        if(head < tail){
+            tail++;
+            head = tail-1;
+        }
+        else{
+            if(arr[tail] == 0)cntzero--;
+            tail++;
+        }
+    }
+    cout << ans << "\n";
 }
 
 signed main()
