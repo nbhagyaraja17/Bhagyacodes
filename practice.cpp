1ff3a59a25d407154d6fd1c14d85ca5b04cebac2
Bhagya <nbhagyaraja17@gmail.com>
2022-07-08 17:58:27 +0530

    find count of nodes invovled in cycle

--- practice.cpp
+++ practice.cpp
@@ -11,12 +11,14 @@ using ll = long long;
 
 const int mod = 1e9 + 7;
 
-// find if there is any cycle in directed graph, print any cycle elements
+// finding the count of nodes that involved in cycle in directed graphs
 vector<vector<int>>g;
 vector<int>col;
 vector<int>parent;
 bool is_cycle =0;
 vector<int>any_cycle;
+vector<int>prefixorder;
+vector<int>cntcylce;
 
 void dfs(int node, int par)
 {
@@ -24,6 +26,7 @@ void dfs(int node, int par)
     parent[node] = par;
     for(auto v : g[node])
     {
+        // if(v == parent[node]) continue;
         if(col[v] == 1)
         {
             dfs(v, node);
@@ -40,22 +43,43 @@ void dfs(int node, int par)
                 any_cycle.push_back(temp);
                 reverse(any_cycle.begin(), any_cycle.end());
             }
+            cntcylce[node]++;
+            cntcylce[parent[v]]--;
             is_cycle = 1;
         }
     }
     col[node] = 3;
+    prefixorder.push_back(node);
 }
 void solve()
 {
     int n, m; cin >> n >> m;
     g.resize(n+1);
+    map<pair<int,int>, int>mp;
     for(int i = 0; i<m; i++)
     {
         int a, b; cin >> a >> b;
         g[a].push_back(b);
+        // g[b].push_back(a); for undirected
+
+        // Handling multi graphs
+        // int a, b; cin >> a >> b;
+        // if(a == b){
+        //     // self loop
+        // }
+        // if(a > b)swap(a, b);
+        // if(mp[{a,b}] > 1){
+        //     // multiple edges b/w a and b nodes
+        // }
+        // else{
+        //     g[a].push_back(b);
+        // // g[b].push_back(a); for undirected
+        // }
     }
     col.assign(n+1, 1);
     parent.assign(n+1, 0);
+    prefixorder.assign(n+1, 0);
+    cntcylce.assign(n+1, 0);
     any_cycle.clear();
     for(int i = 1; i<=n ;i++)
     {
@@ -64,7 +88,17 @@ void solve()
             dfs(i, 0);
         }
     }
-    for(auto i : any_cycle)cout << i << " "; cout << "\n";
+    // for(auto i : any_cycle)cout << i << " "; cout << "\n";
+    int cnt_nodes = 0;
+    for(auto v : prefixorder)
+    {
+        cntcylce[parent[v]] += cntcylce[v];
+    }
+    for(auto v : cntcylce)
+    {
+        if(v > 0)cnt_nodes++;
+    }
+    cout << cnt_nodes << "\n";
 }
 
 signed main()
