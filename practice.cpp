56d48dd5cec358a6b9a7ad77caf6a2e0e89729ce
Bhagya <nbhagyaraja17@gmail.com>
2022-07-18 21:28:36 +0530

    az shortest path II (Floyd Warshall algo)

--- practice.cpp
+++ practice.cpp
@@ -4,6 +4,9 @@ using namespace std;
 #define int long long
 #define vi vector <int>
 using ll = long long;
+using ii = pair<int,int>;
+#define F first
+#define S second
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define deb(x) cout << #x << "=" << x << endl
@@ -11,48 +14,38 @@ using ll = long long;
 
 const int mod = 1e9 + 7;
 
-using ii = pair<int,int>;
-#define F first
-#define S second
-int n, m;
 vector<vector<ii>>g;
-vector<int>dist;
-vector<bool>vis;
+int dist[505][505];
 
-void dijkstra(int sc){
-    dist[sc] = 0;
-    priority_queue<ii>pq;
-    pq.push({-0, sc});
-    while(!pq.empty())
-    {
-        ii fr = pq.top(); pq.pop();
-        int neigh = fr.S;
-        int weigh = -fr.F;
-        if(vis[neigh])continue;
-        vis[neigh] = 1;
-        for(auto v : g[neigh])
-        {
-            if(dist[v.F] > dist[neigh] + v.S)
-            {
-                dist[v.F] = dist[neigh] + v.S;
-                pq.push({-dist[v.F], v.F});
-            }
-        }
-    }
-}
 void solve()
 {
-    cin >> n >> m;
+    int n, m, q;
+    cin >> n >> m >> q;
     g.resize(n+1);
-    dist.assign(n+1, 1e9);
-    vis.assign(n+1, false);
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j <=n ;j++){
+            if(i == j)dist[i][j] = 0;
+            else dist[i][j] = 1e9;
+        }
+    }
     for(int i = 0; i<m; i++){
         int a, b, c; cin >> a >> b >> c;
         g[a].push_back({b,c});
+        g[b].push_back({a,c});
+        dist[a][b] = dist[b][a] = min(dist[a][b], c);
     }
-    dijkstra(1);
-    for(int i = 1; i<=n; i++)cout << dist[i] << " ";
-    cout << "\n";
+    for(int k = 1; k<=n; k++){
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+    while(q--){
+        int x, y; cin >> x >> y;
+        if(dist[x][y] >= 1e9)cout << "-1\n";
+        else cout << dist[x][y] << "\n";
+    }
 }
 
 signed main()
