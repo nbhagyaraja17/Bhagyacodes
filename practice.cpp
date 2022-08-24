dbaf989c820674bf417137f6c4aa40d525e4dcd6
Bhagya <nbhagyaraja17@gmail.com>
2022-08-25 03:20:27 +0530

    seg tree

--- practice.cpp
+++ practice.cpp
@@ -15,53 +15,80 @@ using ii = pair<int,int>;
 const int mod = 1e9 + 7;
 
 int n, m;
-vector<vector<ii>>g;
-vector<pair<ii,int>>edges;
-vector<int>dist;
-vector<bool>vis;
+int hotel_rooms[200002];
+int req_rooms[200002];
+int tr[800008];
 
-void dijkstra(int sc){
-    dist[sc] = 0;
-    priority_queue<ii>pq;
-    pq.push({0,sc});
-    while(!pq.empty()){
-        ii fr = pq.top(); pq.pop();
-        int neigh = fr.S;
-        if(vis[neigh])continue;
-        vis[neigh] = 1;
-        for(auto v : g[neigh]){
-            if(dist[v.F] > dist[neigh] + v.S){
-                dist[v.F] = dist[neigh] + v.S;
-                pq.push({-dist[v.F], v.F});
+// finding index which is greater than given x in given l, r:
+// query format: x, l, r
+// output format: printing index, if not found then print -1
+
+void build(int i, int l , int r){
+    if(l == r){
+        tr[i] = hotel_rooms[l];
+        return;
+    }
+    int mid = (l + r)/2;
+    build(2*i, l, mid);
+    build(2*i + 1, mid + 1, r);
+    tr[i] = max(tr[2*i], tr[2*i + 1]);
+}
+
+void update(int i, int l, int r, int x, int val){
+    if(x < l or x > r)return;
+    if(l == x and r == x){
+        tr[i] -= val;
+        return;
+    }
+    int mid = (l + r)/2;
+    update(2*i, l, mid, x, val);
+    update(2*i + 1, mid + 1, r, x,  val);
+    tr[i] = max(tr[2*i], tr[2*i + 1]);
+}
+
+int query(int i, int l, int r, int lq, int rq, int v)
+{
+    if(r < lq or rq > l)return -1;
+    if(lq <= l and r <= rq)
+    {
+        if(tr[i] <= v)return -1;
+        while(l < r)
+        {
+            int mid = (l + r)/2;
+            if(tr[2*i] > v)
+            {
+                i = 2*i;
+                r = mid;
+            }
+            else
+            {
+                i = 2*i + 1;
+                l = mid + 1;
             }
         }
+        return l;
     }
+    int mid = (l + r)/2;
+    int ls = query(2*i, l, mid, lq, rq, v);
+    if(ls != -1)return ls;
+    return query(2*i + 1, mid + 1, r, lq, rq, v);
 }
+
 void solve()
 {
     cin >> n >> m;
-    g.resize(n+1);
-    dist.assign(n+1, 1e18);
-    vis.assign(n+1, false);
-    for(int i = 0; i<m; i++)
-    {
-        int a, b, c;
-        cin >> a >> b >> c;
-        g[a].push_back({b,c});
-        g[b].push_back({a,c});
-        edges.push_back({{a,b}, c});
+    for(int i = 0; i<n; i++)cin >> hotel_rooms[i];
+    for(int i = 0; i<m; i++)cin >> req_rooms[i];
+    build(1, 0, n-1);
+    for(int i = 0; i<m; i++){
+        int ans = query(1, 0, n-1, req_rooms[i]);
+        if(ans != -1)
+        {
+            update(1, 0, n-1, ans, req_rooms[i]);
+        }
+        cout << ans + 1 << " ";
     }
-    dijkstra(1);
-    int t = -1e18;
-    for(auto v : edges)
-    {
-        int a = v.F.F;
-        int b = v.F.S;
-        int w = v.S;
-        if(abs(dist[a] - dist[b]) == w) t = max(t, max(dist[a], dist[b]) * 10);
-        else t = max(t, (max(dist[a], dist[b]) * 10 + (w - abs(dist[a] - dist[b])) * 5));
-    }
-    cout << t << "\n";
+    cout << "\n";
 }
 
 signed main()
